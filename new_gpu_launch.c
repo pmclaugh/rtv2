386f3d4a37e9c286c10c557f74af158ef002780d
Phil McLaughlin <pnmclaugh@gmail.com>
2018-03-12 13:56:31 -0700

    switching to ubuntu to test ocl20 stuff

--- new_gpu_launch.c
+++ new_gpu_launch.c
@@ -181,7 +181,7 @@ gpu_context *prep_gpu(void)
     	clGetDeviceIDs(gpu->platform[i], CL_DEVICE_TYPE_GPU, gpu->numDevices, &device_ids[offset], &d);
     	gpu->contexts[i] = clCreateContext(0, d, &device_ids[offset], NULL, NULL, &err);
     	for (int j = 0; j < d; j++)
-    		gpu->commands[offset + j] = clCreateCommandQueue(gpu->contexts[i], device_ids[offset + j], CL_QUEUE_PROFILING_ENABLE, &err);
+    		gpu->commands[offset + j] = clCreateCommandQueueWithProperties(gpu->contexts[i], device_ids[offset + j], NULL, &err);
     	offset += d;
     }
     // printf("made contexts and CQs\n");
